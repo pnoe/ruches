a56bce822037136d4a83c755cebdb290f03f2d9d
ooioo <ooioo@free.fr>
2021-03-08 22:53:16 +0100

    AcceptHeaderLocaleResolver pour langage en fonction du navigateur

--- src/main/java/ooioo/ruches/MvcConfig.java
+++ src/main/java/ooioo/ruches/MvcConfig.java
@@ -8,35 +8,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
  * Internationalisation, paramètre lang sur l'url (par ex: ?lang=en)
- * https://www.baeldung.com/spring-boot-internationalization
- * pour utitiliser le header accept language voir :
+ * https://www.baeldung.com/spring-boot-internationalization pour utitiliser le
+ * header accept language voir :
  * https://stackoverflow.com/questions/55736861/retrieve-locale-based-on-the-accept-language-in-spring-boot/55740234
  */
 @Configuration
 @ComponentScan(basePackages = "ooioo.ruches")
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.FRENCH);
-        return slr;
-    }
+	@Bean
+	public LocaleResolver localeResolver() {
+		// Voir aussi SessionLocaleResolver et CookieLocaleResolver
+		AcceptHeaderLocaleResolver lr = new AcceptHeaderLocaleResolver();
+		lr.setDefaultLocale(Locale.FRENCH);
+		return lr;
+	}
 
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
