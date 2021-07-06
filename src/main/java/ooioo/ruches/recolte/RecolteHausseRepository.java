3eaccd8aa2f822469a266f4cec94be65fbdafe54
ooioo <ooioo@free.fr>
2021-07-06 16:43:11 +0200

    liste événments avec hausse de récolte pour mémo (en commentaire)

--- src/main/java/ooioo/ruches/recolte/RecolteHausseRepository.java
+++ src/main/java/ooioo/ruches/recolte/RecolteHausseRepository.java
@@ -1,5 +1,6 @@
 package ooioo.ruches.recolte;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,14 @@ import ooioo.ruches.hausse.Hausse;
 
 @Repository("recolteHausseRepository")
 public interface RecolteHausseRepository extends CrudRepository<RecolteHausse, Long> {
+	
+	/*
+	@Query(value = "select recoltehausse from RecolteHausse recoltehausse where recoltehausse.recolte.date > ?1")
+	Iterable<RecolteHausse> findPeriode(LocalDateTime date);
+	
+	@Query(value = "select recoltehausse from RecolteHausse recoltehausse where recoltehausse.recolte.date > ?1 and recoltehausse.recolte.date < ?2")
+	Iterable<RecolteHausse> findPeriode(LocalDateTime date1, LocalDateTime date2);
+	*/
 	
 	RecolteHausse findByRecolteAndHausse(Recolte recolte, Hausse hausse);
 	
