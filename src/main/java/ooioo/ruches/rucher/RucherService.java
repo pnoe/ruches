4d528762e58cce3fae03d6a69c0e2fe372766255
ooioo <ooioo@free.fr>
2021-10-07 18:13:12 +0200

    RucherService.java simplification inner class static

--- src/main/java/ooioo/ruches/rucher/RucherService.java
+++ src/main/java/ooioo/ruches/rucher/RucherService.java
@@ -43,24 +43,24 @@ public class RucherService {
 	@Value("${rucher.ruche.dispersion}")
 	private String dispersion;
 
-	private static int chSize;
-
 	/*
 	 * Inner class static pour les distances entre ruches
 	 */
 	private static class DataModel {
-		  public final long[][] distanceMatrix = new long[chSize][chSize];
-		  // le nombre de personnes à parcourir les ruches
-		  public final int vehicleNumber = 1;
-		  // l'indice du point de départ
-		  public final int depot = 0;
+		// distanceMatrix est initialisée dans cheminRuchesRucher
+		public static long[][] distanceMatrix;
+		// le nombre de personnes à parcourir les ruches
+		public static final int vehicleNumber = 1;
+		// l'indice du point de départ
+		public static final int depot = 0;
 	}
 
 	/*
 	 * Calcul du chemin le plus court de visite des ruches du rucher
-	 *   https://developers.google.com/optimization/routing/tsp
+	 * https://developers.google.com/optimization/routing/tsp
 	 */
-	public double cheminRuchesRucher(List<RucheParcours> chemin, Rucher rucher, Iterable<Ruche> ruches, boolean redraw) {
+	public double cheminRuchesRucher(List<RucheParcours> chemin, Rucher rucher, Iterable<Ruche> ruches,
+			boolean redraw) {
 		RucheParcours entree = new RucheParcours(0l, 0, rucher.getLongitude(), rucher.getLatitude());
 		chemin.add(entree);
 		int ordre = 0;
@@ -69,60 +69,57 @@ public class RucherService {
 			chemin.add(new RucheParcours(ruche.getId(), ordre, ruche.getLongitude(), ruche.getLatitude()));
 		}
 		int cheminSize = chemin.size();
-		if (cheminSize == 0) { return 0d; }
+		if (cheminSize == 0) {
+			return 0d;
+		}
 		// Initialisation d'un tableau contenant les distances entre ruches
-		chSize = cheminSize;
-		final DataModel data = new DataModel();
+		DataModel.distanceMatrix = new long[cheminSize][cheminSize];
 		// Initialisation de la matrice d'int des distances entre les ruches
-		//  les distances sont en mm
+		// les distances sont en mm
 		double dist;
 		for (int i = 1; i < cheminSize; i++) {
 			for (int j = 0; j < i; j++) {
 				RucheParcours ii = chemin.get(i);
 				RucheParcours jj = chemin.get(j);
 				dist = distance(ii.getLatitude(), jj.getLatitude(), ii.getLongitude(), jj.getLongitude());
-				data.distanceMatrix[i][j] = (int) (dist * 1000.0);
-				data.distanceMatrix[j][i] = data.distanceMatrix[i][j];
+				DataModel.distanceMatrix[i][j] = (int) (dist * 1000.0);
+				DataModel.distanceMatrix[j][i] = DataModel.distanceMatrix[i][j];
 			}
 		}
 		// Initialistion de la diagonale à 0
 		for (int i = 0; i < cheminSize; i++) {
-			data.distanceMatrix[i][i] = 0;
+			DataModel.distanceMatrix[i][i] = 0;
 		}
 		Loader.loadNativeLibraries();
 		// Création du modèle de routage
-		RoutingIndexManager manager =
-		    new RoutingIndexManager(data.distanceMatrix.length, data.vehicleNumber, data.depot);
+		RoutingIndexManager manager = new RoutingIndexManager(DataModel.distanceMatrix.length, DataModel.vehicleNumber,
+				DataModel.depot);
 		RoutingModel routing = new RoutingModel(manager);
 		// création de la callback de calcul de distance utilisant
-		//   la matrice des distances
-		final int transitCallbackIndex =
-			    routing.registerTransitCallback((long fromIndex, long toIndex) -> {
-			      // Convert from routing variable Index to user NodeIndex.
-			      int fromNode = manager.indexToNode(fromIndex);
-			      int toNode = manager.indexToNode(toIndex);
-			      return data.distanceMatrix[fromNode][toNode];
-			    });
+		// la matrice des distances
+		final int transitCallbackIndex = routing.registerTransitCallback((long fromIndex, long toIndex) -> {
+			// Convert from routing variable Index to user NodeIndex.
+			int fromNode = manager.indexToNode(fromIndex);
+			int toNode = manager.indexToNode(toIndex);
+			return DataModel.distanceMatrix[fromNode][toNode];
+		});
 		// Coût du déplacement. Ici le coût est la distance entre deux ruches.
 		routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
 		// Méthode pour calculer le premier parcours
-		//   PATH_CHEAPEST_ARC recherche la ruche la plus proche
-		//   https://developers.google.com/optimization/routing/routing_options#first_sol_options
-		RoutingSearchParameters searchParameters = redraw ? main.defaultRoutingSearchParameters()
-		        .toBuilder()
-		        .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
-		        // advanced search strategy : guided local search
-		        //  pour sortir d'un minimum local
-		        // https://developers.google.com/optimization/routing/routing_options#local_search_options
-		        .setLocalSearchMetaheuristic(LocalSearchMetaheuristic.Value.GUIDED_LOCAL_SEARCH)
-	            // timeLimit obligatoire sinon recherche infinie
-		        //   la recherche durera 10s dans tous les cas
-		        .setTimeLimit(Duration.newBuilder().setSeconds(10).build())
-	            .build()
-			: main.defaultRoutingSearchParameters()
-		        .toBuilder()
-		        .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
-	            .build();
+		// PATH_CHEAPEST_ARC recherche la ruche la plus proche
+		// https://developers.google.com/optimization/routing/routing_options#first_sol_options
+		RoutingSearchParameters searchParameters = redraw
+				? main.defaultRoutingSearchParameters().toBuilder()
+						.setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+						// advanced search strategy : guided local search
+						// pour sortir d'un minimum local
+						// https://developers.google.com/optimization/routing/routing_options#local_search_options
+						.setLocalSearchMetaheuristic(LocalSearchMetaheuristic.Value.GUIDED_LOCAL_SEARCH)
+						// timeLimit obligatoire sinon recherche infinie
+						// la recherche durera 10s dans tous les cas
+						.setTimeLimit(Duration.newBuilder().setSeconds(10).build()).build()
+				: main.defaultRoutingSearchParameters().toBuilder()
+						.setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC).build();
 		// Appelle le solver
 		Assignment solution = routing.solveWithParameters(searchParameters);
 		List<RucheParcours> cheminRet = new ArrayList<>();
@@ -131,9 +128,9 @@ public class RucherService {
 		while (!routing.isEnd(index)) {
 			cheminRet.add(chemin.get(manager.indexToNode(index)));
 			long previousIndex = index;
-		    index = solution.value(routing.nextVar(index));
-		    routeDistance += routing.getArcCostForVehicle(previousIndex, index, 0);
-		    }
+			index = solution.value(routing.nextVar(index));
+			routeDistance += routing.getArcCostForVehicle(previousIndex, index, 0);
+		}
 		cheminRet.add(chemin.get(manager.indexToNode(routing.end(0))));
 		chemin.clear();
 		for (int i = 0; i <= cheminSize; i++) {
@@ -143,8 +140,8 @@ public class RucherService {
 	}
 
 	/**
-	 * Ajoute une liste de ruches dans un rucher.
-	 * Création de l'événement RUCHEAJOUTRUCHER
+	 * Ajoute une liste de ruches dans un rucher. Création de l'événement
+	 * RUCHEAJOUTRUCHER
 	 */
 	public void sauveAjouterRuches(Rucher rucher, String[] ruchesNoms, String date, String commentaire) {
 		LocalDateTime dateEveAjout = LocalDateTime.parse(date, DateTimeFormatter.ofPattern(Const.YYYYMMDDHHMM));
@@ -177,14 +174,14 @@ public class RucherService {
 	}
 
 	/**
-	 * Calcul de la distance entre deux points donnés en latitude, longitude
-	 * Méthode de Haversine, distance orhodromique avec rayon de la terre moyen 6371km
+	 * Calcul de la distance entre deux points donnés en latitude, longitude Méthode
+	 * de Haversine, distance orhodromique avec rayon de la terre moyen 6371km
 	 */
 	public double distance(double lat1, double lat2, double lon1, double lon2) {
 		double sinLatDistance2 = Math.sin(Math.toRadians(lat2 - lat1) / 2d);
 		double sinLonDistance2 = Math.sin(Math.toRadians(lon2 - lon1) / 2d);
-		double a = sinLatDistance2 * sinLatDistance2 + Math.cos(Math.toRadians(lat1))
-				* Math.cos(Math.toRadians(lat2)) * sinLonDistance2 * sinLonDistance2;
+		double a = sinLatDistance2 * sinLatDistance2
+				+ Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * sinLonDistance2 * sinLonDistance2;
 		return 12742000d * Math.asin(Math.sqrt(a));
 		// ou encore : return 12742000d * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 		// 12742000d diamètre de la terre en mètres
