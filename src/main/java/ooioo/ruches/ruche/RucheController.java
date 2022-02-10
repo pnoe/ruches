99f407c8a8fbef93246d98f3a3059f39baaaee80
ooioo <ooioo@free.fr>
2022-02-10 16:28:15 +0100

    thymeleaf javascript inline (replace (\[\[.*\]\]) par /*$1*/ null

--- src/main/java/ooioo/ruches/ruche/RucheController.java
+++ src/main/java/ooioo/ruches/ruche/RucheController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
