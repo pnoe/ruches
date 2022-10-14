a822d1618db8b33b902b6ed262eb363faa7e8454
ooioo <ooioo@free.fr>
2022-10-14 16:02:47 +0200

    récoltes ajout des types de miel : thym et acacia et tilleul

--- src/main/java/ooioo/ruches/recolte/TypeMiel.java
+++ src/main/java/ooioo/ruches/recolte/TypeMiel.java
@@ -3,5 +3,5 @@ package ooioo.ruches.recolte;
 public enum TypeMiel {
 	// en cas de modification, mettre à jour l'internationalisation
 	//  dans messages.properties
-	GARRIGUE, LAVANDE, CHATAIGNIER, TOUTES_FLEURS, ROMARIN
+	GARRIGUE, LAVANDE, CHATAIGNIER, TOUTES_FLEURS, ROMARIN, THYM, ACACIA, TILLEUL
 }
