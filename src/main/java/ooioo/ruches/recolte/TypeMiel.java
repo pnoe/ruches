745f04722eff86b4d784f0471e9ec6f4ebf2e8a1
ooioo <ooioo@free.fr>
2022-10-15 19:03:44 +0200

    GpPluginOpenLayers 3.2.14 -> 3.2.17

--- src/main/java/ooioo/ruches/recolte/TypeMiel.java
+++ src/main/java/ooioo/ruches/recolte/TypeMiel.java
@@ -3,5 +3,7 @@ package ooioo.ruches.recolte;
 public enum TypeMiel {
 	// en cas de modification, mettre à jour l'internationalisation
 	//  dans messages.properties
+	// Attention ne pas changer l'ordre. Il est utilisé en base de donnée.
+	// 0 GARRIGUE, 1 LAVANDE, 2 CHATAIGNIER, 3 TOUTES_FLEURS, 4 ROMARIN, 5 THYM, 6 ACACIA, 7 TILLEUL
 	GARRIGUE, LAVANDE, CHATAIGNIER, TOUTES_FLEURS, ROMARIN, THYM, ACACIA, TILLEUL
 }
