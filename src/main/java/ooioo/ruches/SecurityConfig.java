eaa9931fd72884b7a51d479aa087185f4a34a889
ooioo <ooioo@free.fr>
2023-01-07 15:17:30 +0100

    commentaires pour modifs spring boot 3

--- src/main/java/ooioo/ruches/SecurityConfig.java
+++ src/main/java/ooioo/ruches/SecurityConfig.java
@@ -29,33 +29,42 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 public class SecurityConfig { // extends WebSecurityConfigurerAdapter {
 
 	private final Logger loggerSecConfig = LoggerFactory.getLogger(SecurityConfig.class);
-	
+
 	@Autowired
 	MessageSource messageSource;
-	
+
 	@Resource(name = "userDetailService")
 	private UserDetailsService userDetailsService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		/*
+		 * Pour Spring Boot 3 
+		 * http.authorizeHttpRequests()
+		 * .requestMatchers("/forgotPassword", "/resetPassword", "/resetPasswordFin",
+		 * "/", "/css/**", "/js/**", "/images/**", "/doc/**", "/font/**")
+		 */
 		http.authorizeRequests()
 				.antMatchers("/forgotPassword", "/resetPassword", "/resetPasswordFin", "/", "/css/**", "/js/**",
 						"/images/**", "/doc/**", "/font/**")
-				.permitAll().anyRequest().authenticated()
-				.and().formLogin().loginPage("/login").permitAll()
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
 				.successHandler(new SavedRequestAwareAuthenticationSuccessHandler() {
-				    @Override
-				    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-				            Authentication authentication) throws IOException, ServletException {
-				        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-				        loggerSecConfig.info(messageSource.getMessage("CONNECTE", null,
-				        		LocaleContextHolder.getLocale()), userDetails.getUsername());
-				        super.onAuthenticationSuccess(request, response, authentication);
-				    }                      
-				})
-				.and().logout().permitAll()
+					@Override
+					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+							Authentication authentication) throws IOException, ServletException {
+						UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+						loggerSecConfig.info(
+								messageSource.getMessage("CONNECTE", null, LocaleContextHolder.getLocale()),
+								userDetails.getUsername());
+						super.onAuthenticationSuccess(request, response, authentication);
+					}
+				}).and().logout().permitAll()
 				// désactivation du csrf pour l'api rest
 				.and().csrf().ignoringAntMatchers("/rest/**");
+		/*
+		 * Pour Spring Boot 3 
+		 * .and().csrf().ignoringRequestMatchers("/rest/**");
+		 */
 		return http.build();
 	}
 
