80f57882d3ae4ce99e00755375bc4c43071564c4
ooioo <ooioo@free.fr>
2023-02-17 19:05:34 +0100

    TestEssaim ajouté essaimage

--- src/test/java/ooioo/ruches/selenium/TestEssaim.java
+++ src/test/java/ooioo/ruches/selenium/TestEssaim.java
@@ -23,7 +23,6 @@ import org.openqa.selenium.By;
  * Enchainement de tests :
  *  création ruche, création rucher, ajout ruche dans rucher,
  *  création essaim, ajout essaim dans ruche
- *  A faire : dispersion essaim
  */
 
 @TestMethodOrder(OrderAnnotation.class)
@@ -40,6 +39,7 @@ public class TestEssaim {
 	static String rucherId;
 	static String hausseId;
 	static String essaimId;
+	static String essaimIdNew;
 
 	@BeforeAll
 	static void initChrome() {
@@ -117,7 +117,7 @@ public class TestEssaim {
 		String dateS = driver.findElement(By.name("date")).getAttribute("value");
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 		LocalDateTime dateTime = LocalDateTime.parse(dateS, formatter);
-		dateTime = dateTime.plusMinutes(1);
+		dateTime = dateTime.plusMinutes(2);
 		TestUtils.clearSend(driver, "date", dateTime.format(
 				DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")));
 		TestUtils.submit(driver);
@@ -156,5 +156,23 @@ public class TestEssaim {
 		TestUtils.submit(driver);
 		assertEquals("div", driver.findElement(By.id("detailEssaim")).getTagName());
 	}
+	
+	@Test
+	@Order(7)
+	@DisplayName("Essaimage")
+	void essaimage() {
+		// Attention écriture en base de données
+		driver.get(baseUrl + "essaim/" + essaimId);
+		assertEquals("div", driver.findElement(By.id("detailEssaim")).getTagName());
+		driver.findElement(By.id("essaime")).click();
+		assertEquals("form", driver.findElement(By.id("essaimeForm")).getTagName());
+		driver.findElement(By.name(commentaire)).sendKeys(" - " + comment);
+		TestUtils.submit(driver);
+		String urlDetail = driver.getCurrentUrl();
+		essaimIdNew = urlDetail.substring(urlDetail.lastIndexOf("/") + 1);
+		assertEquals("div", driver.findElement(By.id("detailEssaim")).getTagName());
+	}
+	
+	
 
 }
