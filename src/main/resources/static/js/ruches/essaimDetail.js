e787ca2d0fab38de635dbf1fca4a031df2db77b2
ooioo <ooioo@free.fr>
2023-04-20 15:23:04 +0200

    essaim clone correction bug si annulation

--- src/main/resources/static/js/ruches/essaimDetail.js
+++ src/main/resources/static/js/ruches/essaimDetail.js
@@ -34,7 +34,13 @@ function essaimDetail() {
 	});
 
 	$("#clone").on('click', function() {
-		/*[- Saisie des couples nomEssaim;nomRuche séparés par une "," -]*/
+		function annule() {
+			while (iajout--) {
+				essaimnoms.pop();
+			}
+			ruchesvidesnoms.push(...ruchesvidesmem);
+		}
+		// Saisie des couples nomEssaim;nomRuche séparés par une ","
 		const noms = prompt(nomessaimsvirgule);
 		if (noms == null || noms == "") {
 			return;
@@ -43,46 +49,57 @@ function essaimDetail() {
 		const tabNomOk = [];
 		const tabNomRucheOk = [];
 		const tabNomRucheIncorrect = [];
-		// filter : on ignore ",," ou ",  ,"  
+
+		// pour mémo ajout essaim dans essaimnoms
+		let iajout = 0;
+		// pour retrait ruche vide
+		const ruchesvidesmem = []; 
+
+		//  filter : on ignore ",," ou ",  ,"
 		const nomsarr = noms.split(',').filter(s => s.trim());
 		for (const item of nomsarr) {
 			const arr = item.split(';');
-			/*[- ne nom de l'essaim -]*/
+			// ne nom de l'essaim
 			const ne = arr[0].trim();
-			/*[- si le nom de ruche est vide ou blanc, on passe à la ruche suivante -]*/
+			// Si le nom de ruche est vide ou blanc, on passe à la ruche suivante
 			if ("" === ne) { continue; }
 			let nr = "";
-			/*[- nr nom de la ruche -]*/
+			// nr nom de la ruche
 			if (arr.length > 1) {
 				nr = arr[1].trim();
 			}
 			if ($.inArray(ne, essaimnoms) != -1) {
-				/*[- l'essaim ne existe déjà -]*/
+				// l'essaim ne existe déjà
 				tabNomExiste.push(ne);
 				if ((nr != "") && ($.inArray(nr, ruchesvidesnoms) == -1)) {
-					/*[- ruche nr incorrecte -]*/
+					// ruche nr incorrecte
 					tabNomRucheIncorrect.push(nr);
 				}
 			} else {
-				/*[- on crée l'essaim ne -]*/
+				// on crée l'essaim ne
 				tabNomOk.push(ne);
-				/*[- ce nom ne doit pas être réutilisé -]*/
+				// ce nom ne doit pas être réutilisé
 				essaimnoms.push(ne);
+				iajout++;
 				if (nr == "") {
-					/*[- pas de ruche -]*/
+					// pas de ruche
 					tabNomRucheOk.push("");
 				} else if ($.inArray(nr, ruchesvidesnoms) == -1) {
-					/*[- ruche nr incorrecte -]*/
+					// ruche nr incorrecte
 					tabNomRucheOk.push("");
 					tabNomRucheIncorrect.push(nr);
 				} else {
-					/*[- on met l'essaim ne dans la ruche nr -]*/
+					// on met l'essaim ne dans la ruche nr
 					tabNomRucheOk.push(nr);
-					/*[- ce nom ne doit pas être réutilisé, on l'enlève de la
-						liste des ruches vides -]*/
+					// ce nom ne doit pas être réutilisé, on l'enlève de la
+					//	liste des ruches vides
 					const index = ruchesvidesnoms.indexOf(nr);
 					if (index > -1) {
 						ruchesvidesnoms.splice(index, 1);
+
+						// IL faudra la remettre si annulation du clone...
+						ruchesvidesmem.push(nr);
+
 					}
 				}
 			}
@@ -94,15 +111,20 @@ function essaimDetail() {
 		if (tabNomExiste.length > 0) {
 			if (prompterreur != "") { prompterreur += '\n'; }
 			prompterreur += Essaimstxt + ' : ' + tabNomExiste.join(',') + ' ' + nomexistdejatxt;
-			if (tabNomOk.length == 0) {
+			if (tabNomOk.length === 0) {
 				alert(prompterreur);
+				// Pas d'essaim ajouté
 				return false;
 			}
 			if (!confirm(prompterreur + '. ' + creertxt + ' ' + tabNomOk.join(',') + ' ?')) {
+				annule();
 				return false;
 			}
 		} else if (tabNomRucheIncorrect.length > 0) {
+			// Il n'y a pas d'erreur sur les noms d'essaim mais il y en a sur les 
+			//   noms de ruche
 			if (!confirm(prompterreur + '. ' + creertxt + ' ' + tabNomOk.join(',') + ' ?')) {
+				annule();
 				return false;
 			}
 		}
