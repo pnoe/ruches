4fd9175a1fe87ed544727f432298276200770830
ooioo <ooioo@free.fr>
2023-07-06 15:43:07 +0200

    RucheControllerMAJ factorisation du code transhumances et améliorations

--- src/main/java/ooioo/ruches/rucher/RucherControllerMAJ.java
+++ src/main/java/ooioo/ruches/rucher/RucherControllerMAJ.java
@@ -1,8 +1,6 @@
 package ooioo.ruches.rucher;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -15,10 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ooioo.ruches.Nom;
 import ooioo.ruches.evenement.Evenement;
 import ooioo.ruches.evenement.EvenementRepository;
-import ooioo.ruches.ruche.RucheRepository;
 
 @Controller
 @RequestMapping("/rucher")
@@ -30,13 +26,15 @@ public class RucherControllerMAJ {
 
 	@Autowired
 	private RucherRepository rucherRepository;
-	@Autowired
-	private RucheRepository rucheRepository;
+	// @Autowired
+	// private RucheRepository rucheRepository;
 	@Autowired
 	private EvenementRepository evenementRepository;
+	@Autowired
+	private RucherService rucherService;
 
 	/**
-	 * Mise à jour eve ruche ajout rucher avec id des ruchers de provenance
+	 * Mise à jour eve "ruche ajout rucher" avec noms des ruchers de provenance.
 	 */
 	@GetMapping("/majValeurEveRAR")
 	public String historiques(Model model) {
@@ -49,75 +47,9 @@ public class RucherControllerMAJ {
 			// On ne peut exclure le dépôt qui sert pour trouver les retraits d'un
 			// rucher vers le dépôt
 			List<Evenement> evensRucheAjout = evenementRepository.findAjoutRucheOK();
-			// Les nom des ruches présentes dans le rucher
-			Collection<Nom> nomRuchesX = rucheRepository.findNomsByRucherId(rucher.getId());
-			List<String> ruches = new ArrayList<>();
-			for (Nom nomR : nomRuchesX) {
-				ruches.add(nomR.nom());
-			}
 			List<Transhumance> histo = new ArrayList<>();
-			for (int i = 0, levens = evensRucheAjout.size(); i < levens; i++) {
-				Evenement eve = evensRucheAjout.get(i);
-				if (eve.getRucher().getId().equals(rucher.getId())) {
-					// si l'événement est un ajout dans le rucher
-					// on retire après l'affichage la ruche de l'événement
-					// de la liste des ruches du rucher
-					// On cherche l'événement précédent ajout de cette ruche
-					// pour indication de sa provenance
-					Evenement evePrec = null;
-					for (int j = i + 1; j < levens; j++) {
-						if ((evensRucheAjout.get(j).getRuche().getId().equals(eve.getRuche().getId()))
-								&& !(evensRucheAjout.get(j).getRucher().getId().equals(rucher.getId()))) {
-							// si (evensRucheAjout.get(j).getRuche().getId().equals(rucherId))
-							// c'est une erreur, deux ajouts successifs dans le même rucher
-							evePrec = evensRucheAjout.get(j);
-							break;
-						}
-					}
-					histo.add(new Transhumance(rucher, true, // type = true Ajout
-							eve.getDate(),
-							Collections.singleton(evePrec == null ? "Inconnue" : evePrec.getRucher().getNom()),
-							Arrays.asList(eve.getRuche().getNom()), new ArrayList<>(ruches), eve.getId()));
-					if (!ruches.remove(eve.getRuche().getNom())) {
-						logger.error("Événement {} le rucher {} ne contient pas la ruche {}", eve.getDate(),
-								eve.getRucher().getNom(), eve.getRuche().getNom());
-					}
-				} else {
-					// l'événenemt eve ajoute une ruche dans un autre rucher
-					// On cherche l'événement précédent ajout de cette ruche
-					for (int j = i + 1; j < levens; j++) {
-						Evenement eveJ = evensRucheAjout.get(j);
-						if (eveJ.getRuche().getId().equals(eve.getRuche().getId())) {
-							if (eveJ.getRucher().getId().equals(rucher.getId())) {
-								// si l'événement précédent evePrec était un ajout dans le
-								// rucher, alors eve retire la ruche du rucher
-								if (!ruches.contains(eve.getRuche().getNom())) {
-									// si l'événement précédent evePrec était un ajout dans le
-									// rucher, alors eve retire la ruche du rucher
-									histo.add(new Transhumance(rucher, false, // type = false Retrait
-											eve.getDate(), Collections.singleton(eve.getRucher().getNom()),
-											Arrays.asList(eve.getRuche().getNom()), new ArrayList<>(ruches),
-											eve.getId()));
-									ruches.add(eve.getRuche().getNom());
-								}
-								break;
-							} else {
-								// c'est un événement ajout dans la ruche mais
-								// dans un autre rucher. IL y a deux événements
-								// successifs ajout de la ruche dans un autre rucher
-								// on revient à la boucle principale qui traitera
-								// ce deuxième événement
-								break;
-							}
-						}
-					}
-				}
-			}
+			rucherService.transhum(rucher, evensRucheAjout, false, histo, null);
 			histoAll.addAll(histo);
-			if (!ruches.isEmpty()) {
-				logger.error(
-						"Historique : après traitement des événements en reculant dans le temps, le rucher n'est pas vide");
-			}
 		}
 		// On trie la liste à afficher par date et on l'ajoute au model
 		Collections.sort(histoAll, (b, a) -> a.date().compareTo(b.date()));
@@ -130,9 +62,7 @@ public class RucherControllerMAJ {
 	/**
 	 * Mise à jour des événements RUCHEAJOUTRUCHER avec le champ valeur contenant le
 	 * nom du rucher de provenance. Ces noms sont récupérés dans le calcul des
-	 * transhumances de tous les ruchers. En commentaire mise de l'id du rucher dans
-	 * le champ valeur mais abandonné car gestion de l'affichage lourde
-	 * (transformation ids en noms)
+	 * transhumances de tous les ruchers.
 	 *
 	 * @param histoAll les transhumances de tous les ruchers
 	 */
@@ -145,22 +75,16 @@ public class RucherControllerMAJ {
 					// Lire le rucher de provenance dans la transhumance
 					// set contenant un rucher unique
 					Rucher ruTr = rucherRepository.findByNom(tr.destProv().iterator().next());
-					if (ruTr == null) {
-						logger.error("Pas de rucher de nom ---{}---", tr.destProv().iterator().next());
-						// deuxième chance, est-ce que le nom du rucher est déjà dans l'eve
-						Rucher ruEve = rucherRepository.findByNom(eve.getValeur());
-						if (ruEve != null) {
-							eve.setValeur(ruEve.getNom());
+					if (ruTr != null) {
+						// On met dans la valeur de l'événmeent ajout, le nom du rucher si différent
+						//   de celui trouvé par l'analyse des transhumances
+						if (!eve.getValeur().equals(ruTr.getNom())) {
+							// mémo val avant save eve pour log
+							String val = eve.getValeur();
+							eve.setValeur(ruTr.getNom());
 							evenementRepository.save(eve);
-						} else {
-							// en fait null ou blanc
-							logger.error("Caramba rien dans l'eve {}", eve.getValeur());
+							logger.info("{} provenance {} corrigée", eve, val);
 						}
-					} else {
-						// lire dans la transhumance l'événement tr.eveId()
-						// On met dans valeur le nom du rucher
-						eve.setValeur(ruTr.getNom());
-						evenementRepository.save(eve);
 					}
 				} else {
 					logger.error("Eve transhumance null !");
