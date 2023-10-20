58aeda952cc96c7201ccc59604375e533aeffa3b
ooioo <ooioo@free.fr>
2023-10-20 11:50:43 +0200

    tsp ajout test valeur min trouvée

--- src/main/java/ooioo/ruches/rucher/TSPService.java
+++ src/main/java/ooioo/ruches/rucher/TSPService.java
@@ -21,48 +21,40 @@ public class TSPService {
 	 */
 	public double cheminRuchesRucher(List<RucheParcours> chemin, Rucher rucher, List<Ruche> ruches, boolean redraw) {
 		// TODO redraw true : augmenter nombre d'itérations
+		// TODO utiliser matrice de distances de cheminRuchesRucherRecuit
 		RucheParcours entree = new RucheParcours(0l, 0, rucher.getLongitude(), rucher.getLatitude());
 		chemin.add(entree);
 		int ordre = 0;
-
-		// TODO initialiser en prenant la ruche la plus proche de la précédente
+		// Parcours initial : choix de la ruche la plus proche de la précédente.
 		List<Ruche> ruchesTri = new ArrayList<>(ruches);
-
 		double diametreTerre = ((rucher.getAltitude() == null) ? 0 : rucher.getAltitude())
 				+ 2 * Utils.rTerreLat(rucher.getLatitude());
-
-		for (int i = 1; i < ruches.size(); i++) {
+		for (int i = 0; i < ruches.size(); i++) {
+			// i indice dans chemin. i = 0 pour l'entrée ajoutée précédemment.
+			//  i < ruches.size() on ajoute toutes les ruches à chemin. 
 			// rechercher dans ruchesTri la ruche la plus proche de la précédente
 			double distMin = Double.MAX_VALUE;
 			double d;
 			int iMin = 0;
 			int rIndex = 0;
 			for (Ruche ruche : ruchesTri) {
-				d = Utils.distance(diametreTerre, ruche.getLatitude(), chemin.get(i - 1).latitude(),
-						ruche.getLongitude(), chemin.get(i - 1).longitude());
+				d = Utils.distance(diametreTerre, ruche.getLatitude(), chemin.get(i).latitude(),
+						ruche.getLongitude(), chemin.get(i).longitude());
 				if (d < distMin) {
 					distMin = d;
 					iMin = rIndex;
 				}
 				rIndex++;
 			}
+			// On ajoute au chemin la ruche d'inde iMin dans le tableau ruchesTri.
+			ordre++;
 			chemin.add(new RucheParcours(ruchesTri.get(iMin).getId(), ordre, ruchesTri.get(iMin).getLongitude(),
 					ruchesTri.get(iMin).getLatitude()));
 			ruchesTri.remove(iMin);
 		}
-		// ajouter la dernière ruche qui reste dans ruchesTri
-		chemin.add(new RucheParcours(ruchesTri.get(0).getId(), ordre, ruchesTri.get(0).getLongitude(),
-				ruchesTri.get(0).getLatitude()));
-
-		/*
-		 * for (Ruche ruche : ruches) { ordre += 1; chemin.add(new
-		 * RucheParcours(ruche.getId(), ordre, ruche.getLongitude(),
-		 * ruche.getLatitude())); }
-		 */
-
 		int cheminSize = chemin.size();
 		if (cheminSize > 1) {
-			ordre += 1;
+			ordre++;
 			RucheParcours fin = new RucheParcours(0l, ordre, rucher.getLongitude(), rucher.getLatitude());
 			chemin.add(fin);
 			cheminSize += 1;
@@ -79,7 +71,7 @@ public class TSPService {
 	 * https://en.wikipedia.org/wiki/Simulated_annealing
 	 */
 	private double cheminRuchesRucherRecuit(List<RucheParcours> chemin, int cheminSize, double diametreTerre) {
-		// Initialisation d'un tableau contenant les distances entre ruches
+		// Initialisation d'un tableau contenant les distances entre les ruches (indices = ordre dans chemin).
 		double[][] distRuches = new double[cheminSize][cheminSize];
 		double dist;
 
@@ -102,7 +94,9 @@ public class TSPService {
 		int iRuche;
 		int jRuche;
 		int nbRuches = cheminSize - 2;
-		// Random random = new Random();
+		
+		double testmin = 1000000000d;
+		
 		while (tCurr > tMin) {
 			iRuche = random.nextInt(nbRuches) + 1;
 			jRuche = random.nextInt(nbRuches) + 1;
@@ -113,6 +107,14 @@ public class TSPService {
 			ef = this.cheminDistance(chemin, distRuches, cheminSize);
 			if (ef <= ec) {
 				ec = ef;
+				
+				// test
+				if (ec < testmin) { 
+					testmin = ec; 
+					// il faudrait éventuellement enregistrer ce chemin minimum
+					//   et l'envoyer en retour de la fonction
+				}
+				
 			} else {
 				if (Math.random() > Math.exp((ec - ef) / tCurr)) {
 					Collections.swap(chemin, iRuche, jRuche);
@@ -123,6 +125,10 @@ public class TSPService {
 			t += 1d;
 			tCurr = t0 / Math.exp(t / tau);
 		}
+		
+		// test
+		System.out.println("Min calculé " + ec + " Min réèl " + testmin);
+		
 		return ec;
 	}
 
