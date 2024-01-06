82fc6cfb79faf1c0f2c2d7fc2f7667bd67605b42
ooioo <ooioo@free.fr>
2024-01-06 16:53:56 +0100

    organize imports

--- src/main/java/ooioo/ruches/SecurityConfig.java
+++ src/main/java/ooioo/ruches/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
