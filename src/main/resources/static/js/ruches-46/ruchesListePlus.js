396326c4228442f18f49af2ffb82647d0b8ac39f
ooioo <ooioo@free.fr>
2024-03-16 15:00:02 +0100

    ruchesListePlus recherche footer

--- src/main/resources/static/js/ruches-46/ruchesListePlus.js
+++ src/main/resources/static/js/ruches-46/ruchesListePlus.js
@@ -1,44 +1,8 @@
 /* globals
-	Ruches, buttTxtPrint, buttTxtCol, DataTable
+	Ruches, buttTxtPrint, buttTxtCol, dtListe
 */
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('rucher').addEventListener('change', event => {
-		table.columns(4).search(event.target.value).draw();
-	});
-	const table = new DataTable('#ruchesplus', {
-		scrollX: true,
-		layout: {
-			topStart: {
-				buttons: [
-					'csv',
-					{
-						extend: 'pdfHtml5',
-						exportOptions: { columns: ':visible' },
-						customize: function(doc) {
-							const sru = document.getElementById('rucher');
-							const valr = sru.options[sru.selectedIndex].value;
-							let title = Ruches + ' ' + (new Date()).toLocaleDateString() +
-								(table.search().length === 0 ? '' : ' <' + table.search() + '>') +
-								(valr === '' ? '' : ' [' + valr + ']');
-							const inputSearch = table.search();
-							if (inputSearch.length !== 0) {
-								title += ' <' + inputSearch + '>';
-							}
-							doc.content[0].text = title;
-						},
-						orientation: 'landscape'
-					},
-					{
-						extend: 'print',
-						text: buttTxtPrint,
-						exportOptions: { columns: ':visible' },
-						orientation: 'landscape'
-					},
-					{ extend: 'colvis', text: buttTxtCol },
-					'pageLength'
-				]
-			}
-		}
-	});
+	// dtListe est dans include.js
+	dtListe('ruchesplus', Ruches, buttTxtPrint, buttTxtCol, 'landscape');
 });
