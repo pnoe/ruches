1272d793a7bf328d802b4b580fcdc5348cbd697c
ooioo <ooioo@free.fr>
2024-11-22 15:18:14 +0100

    TypeEvenement.java mise à jour commentaires

--- src/main/java/ooioo/ruches/evenement/TypeEvenement.java
+++ src/main/java/ooioo/ruches/evenement/TypeEvenement.java
@@ -1,15 +1,13 @@
 package ooioo.ruches.evenement;
 
 /*
- * Le type d'événement
+ * Le type d'événement.
  * Attention ne pas changer l'ordre. Il est utilisé en base de donnée.
  *   0 RUCHEAJOUTRUCHER, 1 AJOUTESSAIMRUCHE
  *   2 HAUSSEPOSERUCHE, 3 HAUSSERETRAITRUCHE, 4 HAUSSEREMPLISSAGE
  *   5 ESSAIMTRAITEMENT, 6 ESSAIMTRAITEMENTFIN, 7 ESSAIMSUCRE
  *   8 COMMENTAIRERUCHE, 9 COMMENTAIREHAUSSE, 10 COMMENTAIREESSAIM, 11 COMMENTAIRERUCHER
- *   13 RUCHEPESEE, 14 RUCHECADRE
- *    l'événement 12 anciennnement dispersion a été intégré à l'entité essaim
- *    attention evenementForm.html saute LIBRE dans la liste des options du select
+ *   12 RUCHEPESEE, 13 RUCHECADRE
  */
 public enum TypeEvenement {
 	RUCHEAJOUTRUCHER, AJOUTESSAIMRUCHE,
